cc6a12ba596fff424b4a198a848a984ce47d21fc
Jeroen Peters <jeroen.peters1@hotmail.nl>
2017-02-14 09:26:31 +0100

    the begin

---
+++ src/logic/Aantal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+/**
+ *
+ * @author Jeroe
+ */
+public class Aantal implements LogicImplementation
+{
+
+    @Override
+    public void Bewerk(String text)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
